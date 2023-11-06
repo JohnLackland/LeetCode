a903525b72239ee215eb2d89de21dc072866ef2a
John Lackland <frksjns96@gmail.com>
2023-11-06 23:00:03 +0100

    Add GroupOfAnagrams sol

---
+++ RoadmapExercises/ArraysAndHashes/GroupOfAnagrams.cs
@@ -0,0 +1,24 @@
+﻿namespace RoadmapExercises.ArraysAndHashes
+{
+    //https://leetcode.com/problems/group-anagrams/
+    public static class GroupOfAnagrams
+    {
+        public static IList<IList<string>> Logic1(string[] strs)
+        {
+            var map = new Dictionary<string, IList<string>>();
+            foreach (var word in strs)
+            {
+                var wordCharArr = word.ToCharArray();
+                Array.Sort(wordCharArr);
+                var sortedWord = new string(wordCharArr);
+
+                if (!map.TryGetValue(sortedWord, out _))
+                    map[sortedWord] = new List<string>();
+
+                map[sortedWord].Add(word);
+            }
+
+            return map.Values.ToList();
+        }
+    }
+}
