e964dae12f2c9e982720bdfe7d43cd7bdc5ad616
John Lackland <frksjns96@gmail.com>
2023-11-06 21:44:59 +0100

    Add ContainsDuplicate solution

---
+++ RoadmapExercises/ArraysAndHashes/ContainsDuplicate.cs
@@ -0,0 +1,20 @@
+﻿namespace RoadmapExercises.ArraysAndHashes
+{
+    // https://leetcode.com/problems/contains-duplicate/
+    public static class ContainsDuplicate
+    {
+        public static bool Logic1(int[] nums)
+        {
+            var hash = new HashSet<int>();
+            foreach (int i in nums)
+            {
+                if(hash.Contains(i))
+                    return true;
+
+                hash.Add(i);
+            }
+
+            return false;
+        }
+    }
+}
