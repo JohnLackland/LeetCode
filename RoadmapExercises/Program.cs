f4b3c1418b4d93f9094f9a66dd1f079c23007345
John Lackland <frksjns96@gmail.com>
2023-11-07 22:58:57 +0100

    Debug solutions

--- RoadmapExercises/Program.cs
+++ RoadmapExercises/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace RoadmapExercises
+﻿using RoadmapExercises.ArraysAndHashes;
+
+namespace RoadmapExercises
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            TopKFrequentElements.Logic2(new int[] { 1, 2}, 2);
+            ProductExpectSelf.Logic1(new int[] { 1, 2, 3, 4});
         }
     }
 }
