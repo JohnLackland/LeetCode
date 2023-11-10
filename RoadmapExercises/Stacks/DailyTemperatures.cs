d1f7b9226aaa11402db57c5abe499f96f7adbaf1
John Lackland <frksjns96@gmail.com>
2023-11-10 23:52:00 +0100

    Remove Empty line

--- RoadmapExercises/Stacks/DailyTemperatures.cs
+++ RoadmapExercises/Stacks/DailyTemperatures.cs
@@ -7,7 +7,7 @@
         {
             var result = new int[temperatures.Length];
             var stack = new Stack<int>();
-
+            
             for (int i = 0; i < temperatures.Length; i++)
             {
                 while(stack.Count > 0 && temperatures[stack.Peek()] < temperatures[i])
