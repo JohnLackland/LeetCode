aa3b4f6c230b096faf1c9b035a702fd87e53aeb9
John Lackland <frksjns96@gmail.com>
2023-11-10 23:52:18 +0100

    Add debug values

--- RoadmapExercises/Program.cs
+++ RoadmapExercises/Program.cs
@@ -1,12 +1,16 @@
 ﻿using RoadmapExercises.ArraysAndHashes;
+using RoadmapExercises.Stacks;
 
 namespace RoadmapExercises
 {
-    static void Main(string[] args)
+    class Program // This class wraps the Main method
     {
-        Console.WriteLine("Hello, World!");
-        TopKFrequentElements.Logic2(new int[] { 1, 2 }, 2);
-        ProductExpectSelf.Logic1(new int[] { 1, 2, 3, 4 });
-        ValidSudoku.Logic1(new int[] { 1, 2, 3, 4 });
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            TopKFrequentElements.Logic2(new int[] { 1, 2 }, 2);
+            ProductExpectSelf.Logic1(new int[] { 1, 2, 3, 4 });
+            LargestRectangleArea.Logic1(new int[] { 3, 6, 5, 7, 4, 8, 1, 0 });
+        }
     }
 }
