383c42102b39fc94facb90691dcc442a43802c35
John Lackland <frksjns96@gmail.com>
2023-11-10 20:17:41 +0100

    Add reverse polish notation solution

---
+++ RoadmapExercises/Stacks/EvaluateReversePolishNotation.cs
@@ -0,0 +1,41 @@
+﻿namespace RoadmapExercises.Stacks
+{
+    //https://leetcode.com/problems/evaluate-reverse-polish-notation/
+    public static class EvaluateReversePolishNotation
+    {
+        public static int Logic1(string[] tokens)
+        {
+            var stack = new Stack<int>();
+            foreach (var token in tokens)
+            {
+                switch (token)
+                {
+                    case "+":
+                        stack.Push(stack.Pop() + stack.Pop());
+                        break;
+
+                    case "-":
+                        {
+                            var firstNum = stack.Pop();
+                            stack.Push(stack.Pop() - firstNum);
+                            break;
+                        }
+                    case "*":
+                        stack.Push(stack.Pop() * stack.Pop());
+                        break;
+
+                    case "/":
+                        {
+                            var firstNum = stack.Pop();
+                            stack.Push(stack.Pop() / firstNum);
+                            break;
+                        }
+                    default:
+                        stack.Push(Int32.Parse(token));
+                        break;
+                }
+            }
+            return stack.Peek();
+        }
+    }
+}
