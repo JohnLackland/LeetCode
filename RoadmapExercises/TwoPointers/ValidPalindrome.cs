fc8a8baae5c6d74a310b6e1913487be3423b0504
John Lackland <frksjns96@gmail.com>
2023-11-11 13:10:01 +0100

    Add ValidPalindorme solution

---
+++ RoadmapExercises/TwoPointers/ValidPalindrome.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+namespace RoadmapExercises.TwoPointers
+{
+    //https://leetcode.com/problems/valid-palindrome/
+    public static class ValidPalindrome
+    {
+        public static bool Logic1(string s)
+        {
+            var removedNonAlphaNumericChars = s.Where(char.IsLetterOrDigit).ToArray();
+            var left = 0;
+            var right = removedNonAlphaNumericChars.Length - 1;
+
+            while (left < right)
+            {
+                if (char.ToLower(removedNonAlphaNumericChars[left]) != char.ToLower(removedNonAlphaNumericChars[right]))
+                    return false;
+
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+
+        public static bool Logic2(string s)
+        {
+            var strBuilder = new StringBuilder();
+            foreach (var c in s)
+            {
+                if (char.IsLetterOrDigit(c))
+                    strBuilder.Append(char.ToLower(c));
+            }
+
+            var clearedStr = strBuilder.ToString();
+            for (var i = 0; i < clearedStr.Length / 2; i++)
+            {
+                if (clearedStr[i] != clearedStr[clearedStr.Length - 1 - i])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
