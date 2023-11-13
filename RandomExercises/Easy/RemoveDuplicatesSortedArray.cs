d561d631755ddd270f8567cab86ca6309d109cac
John Lackland <frksjns96@gmail.com>
2023-11-13 23:48:04 +0100

    Add RemoveDuplicatesSorted solution

---
+++ RandomExercises/Easy/RemoveDuplicatesSortedArray.cs
@@ -0,0 +1,42 @@
+﻿namespace RandomExercises.Easy
+{
+    // https://leetcode.com/problems/remove-duplicates-from-sorted-array
+    public static class RemoveDuplicatesSortedArray
+    {
+        public static int Logic1(int[] nums)
+        {
+            var hash = new HashSet<int>();
+            foreach (var num in nums)
+            {
+                if (hash.Contains(num))
+                    continue;
+
+                hash.Add(num);
+                nums[hash.Count] = num;
+            }
+            return hash.Count;
+        }
+
+        public static int Logic2(int[] nums)
+        {
+            var i = 1;
+            foreach (var num in nums)
+            {
+                if (nums[i - 1] != num)
+                    nums[i++] = num;
+            }
+            return i;
+        }
+
+        public static int Logic3(int[] nums)
+        {
+            var j = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[j] != nums[i])
+                    nums[++j] = nums[i];
+            }
+            return j + 1;
+        }
+    }
+}
