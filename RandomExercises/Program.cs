77fcc88c4aab9b936447fbd0603057f4572cb2c9
John Lackland <frksjns96@gmail.com>
2023-11-13 23:48:14 +0100

    Add program

---
+++ RandomExercises/Program.cs
@@ -0,0 +1,12 @@
+﻿using RandomExercises.Medium;
+
+namespace RandomExercises
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            RotateArray.Logic1(new int[] { -1, -100, 3, 99 }, 2);
+        }
+    }
+}
