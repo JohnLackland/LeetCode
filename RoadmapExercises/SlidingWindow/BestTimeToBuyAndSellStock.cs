6e676f6628901099137511ff5111f79183e66d38
John Lackland <frksjns96@gmail.com>
2023-11-14 23:08:11 +0100

    Add BestTimeToBuyAndSellStock solution

---
+++ RoadmapExercises/SlidingWindow/BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,21 @@
+﻿namespace RoadmapExercises.SlidingWindow
+{
+    //https://leetcode.com/problems/best-time-to-buy-and-sell-stock
+    public static class BestTimeToBuyAndSellStock
+    {
+        public static int Logic1(int[] prices)
+        {
+            var result = 0;
+
+            var minValue = prices[0];
+            foreach ( var pr in prices )
+            {
+                minValue = Math.Min(minValue, pr);
+                result = Math.Max(result, pr - minValue);
+            }
+
+            return result;
+        }
+
+    }
+}
