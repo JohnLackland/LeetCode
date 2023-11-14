94fb7a5422679f5b9cb21989916244eb99190a41
John Lackland <frksjns96@gmail.com>
2023-11-14 23:06:51 +0100

    Add LengthOfLastWord solution

---
+++ RandomExercises/Easy/LengthOfLastWord.cs
@@ -0,0 +1,10 @@
+﻿namespace RandomExercises.Easy
+{
+    public static class LengthOfLastWord
+    {
+        public static int Logic1(string s)
+        {
+            return s.Split(' ').Last(s => !string.IsNullOrWhiteSpace(s)).Length;
+        }
+    }
+}
