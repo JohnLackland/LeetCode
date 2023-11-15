cb36ca338f11860e3ccc66ab5dd92531d7eff5f5
John Lackland <frksjns96@gmail.com>
2023-11-15 23:06:44 +0100

    Add IsSubsequence solution

---
+++ RandomExercises/Easy/IsSubsequence.cs
@@ -0,0 +1,17 @@
+﻿namespace RandomExercises.Easy
+{
+    //https://leetcode.com/problems/is-subsequence
+    public static class IsSubsequence
+    {
+        public static bool Logic1(string s, string t)
+        {
+            var foundCharsCount = 0;
+            foreach (var c in t)
+            {
+                if (foundCharsCount <= s.Length - 1 && s[foundCharsCount] == c)
+                    foundCharsCount++;
+            }
+            return s.Length == foundCharsCount;
+        }
+    }
+}
