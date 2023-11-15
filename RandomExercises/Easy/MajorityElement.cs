c5969e72c0b02cc1fe442dadaede9b1ed2477ec9
John Lackland <frksjns96@gmail.com>
2023-11-15 23:25:52 +0100

    Add MajorityElements sol

---
+++ RandomExercises/Easy/MajorityElement.cs
@@ -0,0 +1,21 @@
+﻿namespace RandomExercises.Easy
+{
+    //https://leetcode.com/problems/majority-element
+    public static class MajorityElement
+    {
+        public static int Logic1(int[] nums)
+        {
+            var dict = new Dictionary<int, int>();
+            foreach (int num in nums)
+            {
+                dict[num] = dict.GetValueOrDefault(num, 0) + 1;
+
+                if (dict[num] > nums.Length / 2)
+                    return num; 
+            }
+
+            // it will be never reached
+            return default;
+        }
+    }
+}
