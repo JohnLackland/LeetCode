758caa9d3844683bdf387ff178c49aa85f62dd8f
John Lackland <frksjns96@gmail.com>
2023-11-17 23:15:50 +0100

    Add MinSubArrayLength solution

---
+++ RandomExercises/Medium/MinSubArrayLen.cs
@@ -0,0 +1,25 @@
+﻿namespace RandomExercises.Medium
+{
+    public static class MinSubArrayLen
+    {
+        //https://leetcode.com/problems/minimum-size-subarray-sum
+        public static int Logic1(int target, int[] nums)
+        {
+            var result = int.MaxValue;
+            var s = 0;
+            var sum = 0;
+            for (int r = 0; r < nums.Length; r++)
+            {
+                sum += nums[r];
+                while (sum >= target)
+                {
+                    result = Math.Min(result, r - s + 1);
+                    sum -= nums[s];
+                    s++;
+                }
+            }
+
+            return result == int.MaxValue ? 0 : result;
+        }
+    }
+}
