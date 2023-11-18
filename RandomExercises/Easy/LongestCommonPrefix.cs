52627fa3c0c67b56856a7d70a93ed6e9c8967d2e
John Lackland <frksjns96@gmail.com>
2023-11-18 23:27:08 +0100

    Add LongestCommonPrefix solution

---
+++ RandomExercises/Easy/LongestCommonPrefix.cs
@@ -0,0 +1,41 @@
+﻿namespace RandomExercises.Easy
+{
+    //https://leetcode.com/problems/longest-common-prefix
+    public static class LongestCommonPrefix
+    {
+        public static string Logic1(string[] strs)
+        {
+            var shortestString = strs.Min(s => s.Length);
+            var result = "";
+            for (int i = 0; i < shortestString; i++)
+            {
+                var currentChar = strs[0][i];
+                foreach (var str in strs)
+                {
+                    if (str[i] != currentChar)
+                        return result;
+                }
+                result += currentChar;
+            }
+            return result;
+        }
+
+        public static string Logic2(string[] strs)
+        {
+            if (strs.Length == 0) return "";
+
+            string prefix = strs[0];
+
+            for (int i = 1; i < strs.Length; i++)
+            {
+                while (strs[i].IndexOf(prefix) != 0)
+                {
+                    prefix = prefix.Substring(0, prefix.Length - 1);
+                    if (string.IsNullOrEmpty(prefix)) return "";
+                }
+            }
+
+            return prefix;
+        }
+    }
+}
