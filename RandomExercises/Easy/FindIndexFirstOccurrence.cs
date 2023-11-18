2639cc6498d7f4305bc6337fdd881d6679d24fd7
John Lackland <frksjns96@gmail.com>
2023-11-18 23:27:34 +0100

    Add FindIndexFirstOccurrence solution

---
+++ RandomExercises/Easy/FindIndexFirstOccurrence.cs
@@ -0,0 +1,15 @@
+﻿namespace RandomExercises.Easy
+{
+    public static class FindIndexFirstOccurrence
+    {
+        public static int Logic1(string haystack, string needle)
+        {
+            for (int start = 0; start < haystack.Length - needle.Length; start++)
+            {
+                if (needle == haystack.Substring(start, start + needle.Length))
+                    return start;
+            }
+            return -1;
+        }
+    }
+}
