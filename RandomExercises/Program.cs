f5eaba7802ce8e19ea7fe126376920150ecc5fb5
John Lackland <frksjns96@gmail.com>
2023-11-19 23:49:07 +0100

    Add debug code

--- RandomExercises/Program.cs
+++ RandomExercises/Program.cs
@@ -7,6 +7,7 @@ namespace RandomExercises
         static void Main(string[] args)
         {
             RotateArray.Logic1(new int[] { -1, -100, 3, 99 }, 2);
+            SubstringConcatenationAllWords.Logic1("lingmindraboofooowingdingbarrwingmonkeypoundcake", new string[] { "fooo", "barr", "wing", "ding", "wing" });
         }
     }
 }
--- RoadmapExercises/Program.cs
+++ RoadmapExercises/Program.cs
@@ -1,5 +1,7 @@
 ﻿using RoadmapExercises.ArraysAndHashes;
+using RoadmapExercises.SlidingWindow;
 using RoadmapExercises.Stacks;
+using RoadmapExercises.TwoPointers;
 
 namespace RoadmapExercises
 {
@@ -11,6 +13,11 @@ namespace RoadmapExercises
             TopKFrequentElements.Logic2(new int[] { 1, 2 }, 2);
             ProductExpectSelf.Logic1(new int[] { 1, 2, 3, 4 });
             LargestRectangleArea.Logic1(new int[] { 3, 6, 5, 7, 4, 8, 1, 0 });
+            SortedTwoSum.Logic1(new int[] { 2, 7, 11, 15 }, 9);
+            GenerateParenthesis.Logic1(4);
+            TrappingRainWater.Logic1(new int[] { -1, 0, 1, 2, -1, -4 });
+            LongestSubstringWithoutRepeatingCharacters.Logic3("pwwkew");
+            LongestRepeatingCharacterReplacement.Logic1("AABABBA", 1);
         }
     }
 }
