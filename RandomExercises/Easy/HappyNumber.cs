b377d9c93ac464ec71f16e709be8e7ac13bc5ac4
John Lackland <frksjns96@gmail.com>
2023-11-26 23:37:26 +0100

    Add HappyNumber solution

---
+++ RandomExercises/Easy/HappyNumber.cs
@@ -0,0 +1,27 @@
+﻿namespace RandomExercises.Easy
+{
+    //https://leetcode.com/problems/happy-number
+    public static class HappyNumber
+    {
+        public static bool Logic1(int n)
+        {
+            var hash = new HashSet<int>();
+            while (!hash.Contains(n))
+            {
+                hash.Add(n);
+                var temp = 0;
+                while (n != 0)
+                {
+                    var lastDigit = n % 10;
+                    temp += lastDigit * lastDigit;
+                    n /= 10;
+                }
+                if(temp == 1)
+                    return true;
+
+                n = temp;
+            }
+            return false;
+        }
+    }
+}
