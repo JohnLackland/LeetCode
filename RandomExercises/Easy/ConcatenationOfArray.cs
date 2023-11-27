f2eaf87f24d2d9d2fa531de01d33fb3dc3782034
John Lackland <frksjns96@gmail.com>
2023-11-27 23:40:46 +0100

    Add ConcentrationOfArray solution

---
+++ RandomExercises/Easy/ConcatenationOfArray.cs
@@ -0,0 +1,17 @@
+﻿namespace RandomExercises.Easy
+{
+    //https://leetcode.com/problems/concatenation-of-array
+    public static class ConcatenationOfArray
+    {
+        public static int[] Logic1(int[] nums)
+        {
+            var ans = new int[nums.Length * 2];
+            for (var i = 0; i < nums.Length; i++)
+            {
+                ans[i] = nums[i];
+                ans[i + nums.Length] = nums[i];
+            }
+            return ans;
+        }
+    }
+}
