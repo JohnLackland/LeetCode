0206af7e09bd2d9010e13d6068e9a3377595a584
John Lackland <frksjns96@gmail.com>
2023-11-27 23:57:07 +0100

    Add AddTwoNumbers solution

---
+++ RoadmapExercises/LinkedLists/AddTwoNumbers.cs
@@ -0,0 +1,27 @@
+﻿namespace RoadmapExercises.LinkedLists
+{
+    //https://leetcode.com/problems/add-two-numbers
+    public static class AddTwoNumbers
+    {
+        public static ListNode Logic1(ListNode l1, ListNode l2)
+        {
+            var dummy = new ListNode(int.MinValue);
+            var curr = dummy;
+
+            var carry = 0;
+
+            while (l1 != null || l2 != null || carry != 0)
+            { 
+                var nodeSum = (l1?.val ?? 0) + (l2?.val ?? 0) + carry;
+                carry = nodeSum / 10;
+                curr.next = new ListNode(nodeSum % 10);
+
+                curr = curr.next;
+                l1 = l1?.next;
+                l2 = l2?.next;
+            }
+
+            return dummy.next;
+        }
+    }
+}
