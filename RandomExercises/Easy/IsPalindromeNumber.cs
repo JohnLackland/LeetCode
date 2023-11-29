8ffc7f1df80858ac93b4fff7b03bd7dc5372abff
John Lackland <frksjns96@gmail.com>
2023-11-29 23:20:59 +0100

    IsPalindromeNumber

---
+++ RandomExercises/Easy/IsPalindromeNumber.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml.Linq;
+
+namespace RandomExercises.Easy
+{
+    public static class IsPalindromeNumber
+    {
+        public static bool Logic1(int x)
+        {
+            var reversedNumber = 0;
+            var temp = x;
+            while (temp > 0)
+            { 
+                var lastDigit = temp % 10;
+                reversedNumber = reversedNumber * 10 + lastDigit;
+                temp /= 10;
+            }
+            return reversedNumber == x;
+        }
+
+    }
+}
