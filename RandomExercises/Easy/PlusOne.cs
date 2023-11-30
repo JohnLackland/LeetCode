4dc924aab60990f5fd2a05d9d69056c2f94a54d6
John Lackland <frksjns96@gmail.com>
2023-11-30 23:52:28 +0100

    PlusOne solution

---
+++ RandomExercises/Easy/PlusOne.cs
@@ -0,0 +1,26 @@
+﻿using System.Xml.Linq;
+
+namespace RandomExercises.Easy
+{
+    public static class PlusOne
+    {
+        public static int[] Logic1(int[] digits)
+        {
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                if (digits[i] < 9)
+                {
+                    digits[i]++;
+                    return digits;
+                }
+
+                digits[i] = 0;
+            }
+
+            digits = new int[digits.Length + 1];
+            digits[0] = 1;
+            return digits;
+        }
+
+    }
+}
