ab2c04d927eaa0df5b3fe29ace394a76155cec6a
John Lackland <frksjns96@gmail.com>
2023-12-02 23:40:37 +0100

    Add ReorderLinkedList sol

---
+++ RoadmapExercises/LinkedLists/ReorderLinkedList.cs
@@ -0,0 +1,42 @@
+﻿namespace RoadmapExercises.LinkedLists
+{
+    //https://leetcode.com/problems/reorder-list/
+    public static class ReorderLinkedList
+    {
+        //Iterative
+        public static void Logic1(ListNode head)
+        {
+            var slow = head;
+            var fast = head.next;
+            while (fast != null && fast.next != null) 
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            var list2 = slow.next;
+            //slow.next = null;
+            ListNode prev = null;
+
+            while (list2 != null)
+            {
+                var temp = list2.next;
+                list2.next = prev;
+                prev = list2;
+                list2 = temp;
+            }
+
+            var list1 = head;
+            list2 = prev;
+            while(list2 != null)
+            {
+                var temp1 = list1.next;
+                var temp2 = list2.next;
+                list1.next = list2;
+                list2.next = temp1;
+                list1 = temp1;
+                list2 = temp2;
+            }
+        }
+    }
+}
