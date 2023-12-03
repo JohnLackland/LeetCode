ab12ebeaba86bedbf0169d200a48ff0022c03d0e
John Lackland <frksjns96@gmail.com>
2023-12-03 23:52:20 +0100

    Add ReverseWords solution

---
+++ RandomExercises/Medium/ReverseWords.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace RandomExercises.Medium
+{
+    //https://leetcode.com/problems/reverse-words-in-a-string
+    public static class ReverseWords
+    {
+        public static string Logic1(string s)
+        {
+            var splittedWords = s.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            var strBuilder = new StringBuilder();
+
+            for (int i = splittedWords.Length - 1; i >= 0; i--)
+            {
+                strBuilder.Append(splittedWords[i]);
+                if (i != 0)
+                    strBuilder.Append(' ');
+            }
+            return strBuilder.ToString();
+        }
+    }
+}
