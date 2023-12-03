0bd4d099cdbccfeea8f32898881867a5d51264eb
John Lackland <frksjns96@gmail.com>
2023-12-04 00:12:05 +0100

    Add HIndex solution

---
+++ RandomExercises/Medium/HIndex.cs
@@ -0,0 +1,21 @@
+﻿namespace RandomExercises.Medium
+{
+    // https://leetcode.com/problems/h-index
+    public static class HIndex
+    {
+        public static int Logic1(int[] citations)
+        {
+            Array.Sort(citations, (a, b) => b.CompareTo(a));
+            var result = 0;
+            for (int i = 0; i < citations.Length; i++)
+            {
+                if (citations[i] > i)
+                    result++;
+                else
+                    return result;
+            }
+
+            return result;
+        }
+    }
+}
