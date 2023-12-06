e761f855fc7b6fdadc87862b24392881f9f0c4b9
John Lackland <frksjns96@gmail.com>
2023-12-06 21:37:07 +0100

    Add RemoveDuplicates2 solution

---
+++ RandomExercises/Medium/RemoveDuplicates2.cs
@@ -0,0 +1,24 @@
+﻿namespace RandomExercises.Medium
+{
+    public static class RemoveDuplicates2
+    {
+        public static int Logic1(int[] nums)
+        {
+            if (nums.Length <= 2)
+                return nums.Length;
+
+            var slow = 2;
+
+            for (int fast = 0; fast < nums.Length; fast++)
+            {
+                if (nums[slow - 2] != nums[fast])
+                {
+                    nums[slow] = nums[fast];
+                    slow++;
+                }
+            }
+
+            return slow;
+        }
+    }
+}
