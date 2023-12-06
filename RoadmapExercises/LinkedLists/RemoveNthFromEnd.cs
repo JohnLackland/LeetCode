a48bffa6b8fc80a8fc4ed2efc9542e6a0b026a7f
John Lackland <frksjns96@gmail.com>
2023-12-06 21:38:25 +0100

    Remove nth node from end

---
+++ RoadmapExercises/LinkedLists/RemoveNthFromEnd.cs
@@ -0,0 +1,27 @@
+﻿using RoadmapExercises.LinkedLists.Nodes;
+
+namespace RoadmapExercises.LinkedLists
+{
+    public static class RemoveNthFromEnd
+    {
+        public static ListNode Logic1(ListNode head, int n)
+        {
+            var dummy = new ListNode(0, head);
+            var first = dummy;
+            var second = head;
+            for (int i = 0; i < n; i++)
+            {
+                second = second.next;
+            }
+
+            while (second != null)
+            {
+                first = first.next;
+                second = second.next;
+            }
+
+            first.next = first.next.next;
+            return dummy.next;
+        }
+    }
+}
