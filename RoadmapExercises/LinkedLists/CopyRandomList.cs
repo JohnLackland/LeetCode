c65df2e6893c785002f5aed747d51f1ddb321d90
John Lackland <frksjns96@gmail.com>
2023-12-06 21:39:08 +0100

    Add copy random list solution

---
+++ RoadmapExercises/LinkedLists/CopyRandomList.cs
@@ -0,0 +1,29 @@
+﻿using RoadmapExercises.LinkedLists.Nodes;
+
+namespace RoadmapExercises.LinkedLists
+{
+    public static class CopyRandomList
+    {
+        public static Node Logic1(Node head)
+        {
+            var current = head;
+            var dict = new Dictionary<Node, Node>();
+            while (current != null)
+            {
+                var copy = new Node(current.val);
+                dict[current] = copy;
+                current = current.next;
+            }
+
+            current = head;
+            while (current != null)
+            {
+                var copy = dict[current];
+                copy.next = current.next == null ? null : dict[current.next];
+                copy.random = current.random == null ? null : dict[current.random];
+                current = current.next;
+            }
+            return dict[head];
+        }
+    }
+}
