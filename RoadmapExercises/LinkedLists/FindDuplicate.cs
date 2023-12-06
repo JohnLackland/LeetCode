c6151cec1276fbd76991cba4da33007a0aa1e211
John Lackland <frksjns96@gmail.com>
2023-12-06 21:38:50 +0100

    Find duplicates with space O(1)

---
+++ RoadmapExercises/LinkedLists/FindDuplicate.cs
@@ -0,0 +1,29 @@
+﻿namespace RoadmapExercises.LinkedLists
+{
+    //https://leetcode.com/problems/find-the-duplicate-number/
+    public static class FindDuplicate
+    {
+        public static int Logic1(int[] nums)
+        {
+            var fast = 0;
+            var slow = 0;
+            while (true)
+            {
+                slow = nums[slow];
+                fast = nums[nums[fast]];
+                if (fast == slow)
+                    break;
+            }
+
+            slow = 0;
+
+            while(slow != fast) 
+            {
+                slow = nums[slow];
+                fast = nums[fast];
+            }
+
+            return slow;
+        }
+    }
+}
