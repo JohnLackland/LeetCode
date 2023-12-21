3f6a6f7239c4e49f411d719f970f723b47fdcc25
John Lackland <frksjns96@gmail.com>
2023-12-21 17:47:48 +0100

    Add Stack data structure

---
+++ DataStructures/Stack.cs
@@ -0,0 +1,34 @@
+﻿namespace DataStructures
+{
+    public class Stack <T>
+    {
+        private readonly List<T> values;
+
+        public Stack()
+        {
+            values = [];
+        }
+
+        public void Push(T value)
+        {
+            values.Add(value);
+        }
+
+        public T Pop()
+        {
+            if (values.Count > 0)
+            {
+                var topValue = values[^1];
+                values.RemoveAt(values.Count - 1);
+                return topValue;
+            }
+
+            return default;
+        }
+
+        public int Size()
+        {
+            return values.Count;
+        }
+    }
+}
