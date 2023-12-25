af2dc1520f489d7e7c249bf125b7be7c8d890596
John Lackland <frksjns96@gmail.com>
2023-12-25 17:24:59 +0100

    Add Koko'sBanana solution

---
+++ RoadmapExercises/BinarySearch/KokosBanana.cs
@@ -0,0 +1,35 @@
+﻿namespace RoadmapExercises.BinarySearch
+{
+    //https://leetcode.com/problems/koko-eating-bananas/
+    public static class KokosBanana
+    {
+        public static int MinEatingSpeed(int[] piles, int h)
+        {
+            var low = 1;
+            var high = piles.Max();
+            var result = high;
+
+            if (piles.Length == h)
+                return high;
+
+            while (low <= high)
+            {
+                var mid = low + (high - low) / 2;
+                long hoursToFinish = 0;
+
+                foreach (var p in piles)
+                    hoursToFinish += (int)Math.Ceiling((double)p / mid);
+
+                if (hoursToFinish <= h)
+                {
+                    result = mid;
+                    high = mid - 1;
+                }
+                else
+                    low = mid + 1;
+            }
+
+            return result;
+        }
+    }
+}
