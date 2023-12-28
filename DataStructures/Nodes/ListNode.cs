b15b81800e0fed95d3b3986329d091f8cc4c9a6e
John Lackland <frksjns96@gmail.com>
2023-12-28 23:01:01 +0100

    Add duble linked list node

---
+++ DataStructures/Nodes/ListNode.cs
@@ -0,0 +1,15 @@
+﻿namespace DataStructures.Nodes
+{
+    public class ListNode(int val, ListNode next = null)
+    {
+        public int Val { get; set; } = val;
+        public ListNode Next { get; set; } = next;
+    }
+
+    public class DoubleListNode(int val, DoubleListNode next = null, DoubleListNode prev = null)
+    {
+        public int Val { get; set; } = val;
+        public DoubleListNode Next { get; set; } = next;
+        public DoubleListNode Prev { get; set; } = prev;
+    }
+}
