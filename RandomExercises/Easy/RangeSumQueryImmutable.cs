04c092780100483be7bef8e8462b7931c11cf220
John Lackland <frksjns96@gmail.com>
2023-12-28 23:01:57 +0100

    Create RangeSumQueryImmutable solution

---
+++ RandomExercises/Easy/RangeSumQueryImmutable.cs
@@ -0,0 +1,24 @@
+﻿namespace RandomExercises.Easy
+{
+    //https://leetcode.com/problems/range-sum-query-immutable/
+    public class RangeSumQueryImmutable
+    {
+        private static int[] preSums;
+        public RangeSumQueryImmutable(int[] nums)
+        {
+            preSums = nums;
+
+            for (int i = 1; i < nums.Length; i++)
+                preSums[i] += preSums[i - 1];
+        }
+
+        public static int SumRange(int left, int right)
+        {
+            if (left == 0)
+                return preSums[right];
+
+            return preSums[right] - preSums[left -1];
+        }
+
+    }
+}
