c181f34dda9c2e96c1379b304e810f192667cc91
John Lackland <frksjns96@gmail.com>
2023-12-28 23:46:14 +0100

    Add CountStudents solution

---
+++ RandomExercises/Easy/CountStudents.cs
@@ -0,0 +1,38 @@
+﻿namespace RandomExercises.Easy
+{
+    //https://leetcode.com/problems/number-of-students-unable-to-eat-lunch/solutions/
+    public static class CountStudents
+    {
+        public static int Logic1(int[] students, int[] sandwiches)
+        {
+            var studentQueue = new Queue<int>();
+            var sandwichStack = new Stack<int>();
+
+            for (int i = 0; i < students.Length; i++)
+            {
+                studentQueue.Enqueue(students[i]);
+                sandwichStack.Push(sandwiches[sandwiches.Length - 1 - i]);
+            }
+
+            var studentsCouldEat = true;
+            while (studentsCouldEat)
+            {
+                studentsCouldEat = false;
+                for (int i = 0; i < studentQueue.Count; i++)
+                {
+                    var currStudent = studentQueue.Dequeue();
+
+                    if (currStudent == sandwichStack.Peek())
+                    { 
+                        sandwichStack.Pop();
+                        studentsCouldEat = true;
+                    }
+                    else
+                        studentQueue.Enqueue(currStudent);
+                }
+            }
+
+            return studentQueue.Count;
+        }
+    }
+}
