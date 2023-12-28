46a50747bf44c95fdb1de4e822ebf3c20e28f7c7
John Lackland <frksjns96@gmail.com>
2023-12-28 23:03:07 +0100

    Add SubarraySumEqualsK solution

---
+++ RandomExercises/Medium/SubarraySumEqualsK.cs
@@ -0,0 +1,28 @@
+﻿namespace RandomExercises.Medium
+{
+    //https://leetcode.com/problems/subarray-sum-equals-k/
+    public static class SubarraySumEqualsK
+    {
+        public static int Logic1(int[] nums, int k)
+        {
+            var result = 0;
+            var dict = new Dictionary<int, int>
+            {
+                [0] = 1
+            };
+            var sum = 0;
+            foreach (var num in nums) 
+            {
+                sum += num;
+                var diff = sum - k;
+
+                dict.TryGetValue(diff, out int value);
+                result += value;
+
+                dict[diff] = dict.GetValueOrDefault(diff, 0) + 1;
+            }
+
+            return result;
+        }
+    }
+}
