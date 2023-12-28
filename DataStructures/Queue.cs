3f1d560d080e45c3d6df1c33910959b3ab8d0848
John Lackland <frksjns96@gmail.com>
2023-12-28 23:01:18 +0100

    Create Queue data structure

---
+++ DataStructures/Queue.cs
@@ -0,0 +1,67 @@
+﻿using DataStructures.Nodes;
+
+namespace DataStructures
+{
+    public class Queue
+    {
+        private DoubleListNode dummyHead;
+        private DoubleListNode dummyTail;
+
+        public Queue()
+        {
+            dummyHead = new DoubleListNode(0);
+            dummyTail = new DoubleListNode(0);
+            dummyHead.Next = dummyTail;
+            dummyTail.Prev = dummyHead;
+        }
+
+        public bool IsEmpty()
+        {
+            return dummyHead.Next == dummyTail;
+        }
+
+        public void Append(int value)
+        {
+            var newNode = new DoubleListNode(value)
+            {
+                Prev = dummyTail.Prev,
+                Next = dummyTail
+            };
+            dummyTail.Prev.Next = newNode;
+            dummyTail.Prev = newNode;
+        }
+
+        public void appendleft(int value)
+        {
+            var newNode = new DoubleListNode(value)
+            {
+                Next = dummyHead.Next,
+                Prev = dummyHead
+            };
+            dummyHead.Next.Prev = newNode;
+            dummyHead.Next = newNode;
+        }
+
+        public int Pop()
+        {
+            if (IsEmpty())
+                return -1;
+
+            var targetNode = dummyTail.Prev;
+            targetNode.Prev.Next = dummyTail;
+            dummyTail.Prev = targetNode.Prev;
+            return targetNode.Val;
+        }
+
+        public int Popleft()
+        {
+            if (IsEmpty())
+                return -1;
+
+            var targetNode = dummyHead.Next;
+            dummyHead.Next = targetNode.Next;
+            targetNode.Next.Prev = dummyHead;
+            return targetNode.Val;
+        }
+    }
+}
